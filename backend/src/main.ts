1566d9a6aa07eb41ecb3a01926734df74613dade
wesleycremonini <wesley18.cre@gmail.com>
2022-07-11 18:19:56 -0300

    bcrypt and class-validator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(3001);
 }
 bootstrap();
